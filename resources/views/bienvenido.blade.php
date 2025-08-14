aa9cce0c0402328c541fbc445f92ceb69ba7a4ba
Angel Escamilla <juan.gallardo@cecyteg.edu.mx>
2025-08-14 14:37:06 -0600

    correccion

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style type="text/css">
+      html,body{
+        height:100%;
+        padding:0;
+        margin:0;
+      }
+      .oc{
+        display:table;
+        width:100%;
+        height:100%;
+      }
+      .ic{
+        display:table-cell;
+        vertical-align:middle;
+        height:100%;
+      }
+      form{
+        display:block;
+        background:#FFF;
+        border:2px solid #234176;
+        padding:0 0 25px 0;
+        width:500px;
+        font-family:helvetica,sans-serif;
+        font-size:14px;
+        margin:10px auto;
+      }
+      .fel,.fer,.fec{
+        text-align:center;
+        width:350px;
+        margin:0 auto;
+        padding:10px;
+        display: flex;
+        justify-content: space-evenly;
+      }
+      .fel{
+        text-align:left;
+      }
+      .fer{
+        text-align:right;
+      }
+      h1{
+        font-weight:bold;
+        font-size:21px;
+        margin:0;
+        padding:20px 10px;
+        text-align:center;
+      }
+      p{
+        margin:15px auto;
+        width:75%;
+        text-align:left;
+      }
+      ul{
+        margin:15px auto;
+        width:75%;
+      }
+      h2{
+        margin:25px 10px;
+        font-weight:bold;
+        text-align:center;
+      }
+      label,h2{
+        font-size:16px;
+      }
+      .logo{
+        background:#005ee5 center 25px url(%%IMAGE:logo_cecyteg%%) no-repeat;
+        padding-top:180px;
+      }
+      
+      select, input
+      {
+        
+        margin: auto;
+        width: 250px;
+      }
+      
+      label
+      {
+        width: 100px;
+      }
+      
+      button
+      {
+        width: 100px;
+        height: 50px;
+      }
+      
+      h1, h2
+      {
+        color: white;
+      }
+      button
+      {
+        border-radius: 20px;
+        width: 150px;
+        background-color: #005ee5;
+        color: white;
+        border-color: white;
+        
+      }
+
+      a
+      {
+        color: white !important;
+        text-decoration: none;
+      }
+      
+    </style>
+    <title>
+      Firewall Authentication
+    </title>
+  </head>
+  <body>
+    <div class="oc">
+      <div class="ic">
+        <form action="%%AUTH_POST_URL%%" method="post" id="formulario">
+
+          <h1 class="logo" id="mensaje">
+            Bienvenido, tu registro se realizo correctamente
+            </h1>
+          <h2>
+            ya puedes continuar con tu actividad
+          </h2>
+          
+     
+          
+
+          <div class="fer">
+            <button id="btn_enviar" >
+               <a href="https://10.10.10.10/">Ir a 10.10.10.10</a>  
+            </button>
+          </div>
+        </form>
+        
+      </div>
+    </div>
+    
+  </body>
+</html>
--- resources/views/reporte.blade.php
+++ resources/views/reporte.blade.php
@@ -4,8 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    @page {
+        margin: 100px 25px 6px 25px; /* top, right, bottom, left */
+    }
+
+    header {
+        position: fixed;
+        top: -40px;
+        left: 0;
+        right: 0;
+        height: 60px;
+        background-color: #f5f5f5;
+        text-align: center;
+        line-height: 35px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    footer {
+        position: fixed;
+        bottom: -40px;
+        left: 0;
+        right: 0;
+        height: 30px;
+        background-color: #f5f5f5;
+        text-align: center;
+        font-size: 12px;
+        border-top: 1px solid #ccc;
+        line-height: 25px;
+    }
+    </style>
 </head>
 <body>
+
+
+<header>
+    <h3>Reporte de Usuarios</h3>
+</header>
+
+<footer>
+    Juan Jose
+</footer>
+<main>
     <table border="1">
         @foreach ( $registros as $registro )
 
@@ -20,5 +60,8 @@
                 
         @endforeach
     </table>
+</main>
+
+
 </body>
 </html>
