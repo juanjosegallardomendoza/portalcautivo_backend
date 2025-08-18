6708047b2ebd53f675aa66e7b623dc08a1fb2e49
unknown <isc.juan.gallard@gmail.com>
2025-08-18 10:37:15 -0600

    correcciones

--- resources/views/busqueda.blade.php
+++ resources/views/busqueda.blade.php
@@ -275,7 +275,7 @@ va;g.timeInterval=yd;g.timeout=eb;g.timeoutWith=zd;g.timestamp=Ad;g.toArray=mb;g
             document.getElementById(`div_${data.id}`).innerHTML += `<br><div style="user-select: text; margin-left:10px; "><b>${val.propiedad}</b>: ${val.valor} <br></div>`;
             if(val.url!="")
             {
-                document.getElementById(`div_${data.id}`).innerHTML += `<br><div style="user-select: text; margin-left:10px; "><a target="_blank" href="${val.url}"> IR ${val.propiedad}   </a> <br></div> <br>`;
+                document.getElementById(`div_${data.id}`).innerHTML += `<br><div style="user-select: text; margin-left:10px; "><a target="_blank" href="${val.url}"> IR AL PORTAL   </a> <br></div> <br>`;
                 
             }
             document.getElementById(`div_${data.id}`).innerHTML +="<br>";
