66296acf02e12a2abab6aec8a96d1dc3e5bce54b
unknown <isc.juan.gallard@gmail.com>
2025-08-28 11:31:03 -0600

    correcciones

--- app/Models/Registro.php
+++ app/Models/Registro.php
@@ -11,4 +11,24 @@ class Registro extends Model
     {
      return $this->belongsTo(Usuario::class);
     }
+
+    protected static function booted()
+    {
+        static::creating(function ($registro) {
+            $endedAt = Carbon::parse($registro->created_at)->addMinutes($registro->duration);
+
+         
+            $existe = Registro::where('created_at', '<=', $registro->created_at)
+                ->where('ended_at', '>=', $registro->created_at)
+                ->where("usuario_id", "=", $registro->usuario_id)
+                ->where("ip", "=", $registro->ip)
+                ->exists();
+
+            if ($existe) {
+                return false;
+            }
+
+            $registro->ended_at = $endedAt;
+        });
+    }
 }
